277df85787fcfd96d9e7df9088509805b375139a
latortuga71 <latortuga@Christopher-PC.localdomain>
2022-09-03 14:22:56 -0400

    adding list tcp ports

--- internal/modules/enumeration/listports/listports_windows.go
+++ internal/modules/enumeration/listports/listports_windows.go
@@ -4,14 +4,32 @@
 package listports
 
 import (
-	"os/exec"
+	"fmt"
+	"unsafe"
+
+	"github.com/latortuga71/GoC2/pkg/winapi"
 )
 
 func ListPorts() (string, error) {
-	cmd := exec.Command("powershell", "-c", "Get-NetTCPConnection -State Listen")
-	result, cmdError := cmd.CombinedOutput()
-	if cmdError != nil {
-		return "", cmdError
+	var results string
+	var needed uint32
+	err := winapi.GetTcpTable2(nil, &needed, true)
+	if needed == 0 {
+		return "", err
 	}
-	return string(result), nil
+	buffer := make([]byte, needed)
+	bufferPtr := (*winapi.MIB_TCPTABLE2)(unsafe.Pointer(&buffer[0]))
+	index := int(unsafe.Sizeof(bufferPtr.DwNumEntries))
+	step := int(unsafe.Sizeof(bufferPtr.Table))
+	err = winapi.GetTcpTable2(bufferPtr, &needed, true)
+	if err != nil {
+		return "", err
+	}
+	for x := 0; x < int(bufferPtr.DwNumEntries); x++ {
+		t := (*winapi.MIB_TCPROW2)(unsafe.Pointer(&buffer[index]))
+		s := winapi.MIB_TCP_STATE(t.DwState)
+		results += fmt.Sprintf("PID: %d STATE: %s LADDR: %s LPORT: %d RADDR: %s RPORT: %d\n", t.DwOwningPid, winapi.TCPStatuses[s], winapi.ParseIPv4(t.DwLocalAddr), winapi.DecodePort(t.DwLocalPort), winapi.ParseIPv4(t.DwRemoteAddr), winapi.DecodePort(t.DwRemotePort))
+		index += step
+	}
+	return results, nil
 }
--- pkg/winapi/iphlpapi.go
+++ pkg/winapi/iphlpapi.go
@@ -1,16 +1,98 @@
 package winapi
 
 import (
+	"fmt"
+	"net"
 	"strconv"
 	"strings"
 	"syscall"
 	"unsafe"
 )
 
+// https://github.com/sherifeldeeb/win-netstat/blob/efa1aff6aafc/iphlpapi.go#L30
 var (
 	pIphlpapi          = syscall.NewLazyDLL("Iphlpapi.dll")
 	pGetRTTAndHopCount = pIphlpapi.NewProc("GetRTTAndHopCount")
+	procGetTCPTable2   = pIphlpapi.NewProc("GetTcpTable2")
 )
+
+type TCP_CONNECTION_OFFLOAD_STATE uint32
+
+const ANY_SIZE = 1
+
+type MIB_TCPTABLE2 struct {
+	DwNumEntries uint32
+	Table        [ANY_SIZE]MIB_TCPROW2
+}
+
+type MIB_TCPROW2 struct {
+	DwState        uint32
+	DwLocalAddr    uint32
+	DwLocalPort    uint32
+	DwRemoteAddr   uint32
+	DwRemotePort   uint32
+	DwOwningPid    uint32
+	DwOffloadState TCP_CONNECTION_OFFLOAD_STATE
+}
+
+const (
+	TcpConnectionOffloadStateInHost TCP_CONNECTION_OFFLOAD_STATE = iota
+	TcpConnectionOffloadStateOffloading
+	TcpConnectionOffloadStateOffloaded
+	TcpConnectionOffloadStateUploading
+	TcpConnectionOffloadStateMax
+)
+
+type MIB_TCP_STATE int32
+
+var TCPStatuses = map[MIB_TCP_STATE]string{
+	1:  "CLOSED",
+	2:  "LISTEN",
+	3:  "SYN_SENT",
+	4:  "SYN_RECEIVED",
+	5:  "ESTABLISHED",
+	6:  "FIN_WAIT_1",
+	7:  "FIN_WAIT_2",
+	8:  "CLOSE_WAIT",
+	9:  "CLOSING",
+	10: "LAST_ACK",
+	11: "TIME_WAIT",
+	12: "DELETE",
+}
+
+func ParseIPv4(addr uint32) string {
+	return fmt.Sprintf("%d.%d.%d.%d", addr&255, addr>>8&255, addr>>16&255, addr>>24&255)
+}
+
+func ParseIPv6(addr [16]byte) string {
+	var ret [16]byte
+	for i := 0; i < 16; i++ {
+		ret[i] = uint8(addr[i])
+	}
+
+	// convert []byte to net.IP
+	ip := net.IP(ret[:])
+	return ip.String()
+}
+
+func DecodePort(port uint32) uint16 {
+	return syscall.Ntohs(uint16(port))
+}
+
+func GetUintptrFromBool(b bool) uintptr {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func GetTcpTable2(tcpTable *MIB_TCPTABLE2, bufSize *uint32, order bool) (errcode error) {
+	r1, _, _ := syscall.Syscall(procGetTCPTable2.Addr(), 3, uintptr(unsafe.Pointer(tcpTable)), uintptr(unsafe.Pointer(bufSize)), GetUintptrFromBool(order))
+	if r1 != 0 {
+		errcode = syscall.Errno(r1)
+	}
+	return
+}
 
 func inet_addr(ipaddr string) uint32 {
 	var (
