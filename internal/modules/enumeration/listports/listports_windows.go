f124ea2389067685c34a61985e08c9914c5aa81e
latortuga71 <latortuga@Christopher-PC.localdomain>
2022-09-19 10:37:39 -0400

    added exe names to list ports

--- internal/modules/enumeration/listports/listports_windows.go
+++ internal/modules/enumeration/listports/listports_windows.go
@@ -8,6 +8,7 @@ import (
 	"unsafe"
 
 	"github.com/latortuga71/GoC2/pkg/winapi"
+	"golang.org/x/sys/windows"
 )
 
 func ListPorts() (string, error) {
@@ -28,7 +29,19 @@ func ListPorts() (string, error) {
 	for x := 0; x < int(bufferPtr.DwNumEntries); x++ {
 		t := (*winapi.MIB_TCPROW2)(unsafe.Pointer(&buffer[index]))
 		s := winapi.MIB_TCP_STATE(t.DwState)
-		results += fmt.Sprintf("PID: %d STATE: %s LADDR: %s LPORT: %d RADDR: %s RPORT: %d\n", t.DwOwningPid, winapi.TCPStatuses[s], winapi.ParseIPv4(t.DwLocalAddr), winapi.DecodePort(t.DwLocalPort), winapi.ParseIPv4(t.DwRemoteAddr), winapi.DecodePort(t.DwRemotePort))
+		handle, err := windows.OpenProcess(windows.PROCESS_QUERY_INFORMATION|windows.PROCESS_VM_READ, false, t.DwOwningPid)
+		if err != nil {
+			results += fmt.Sprintf("EXE %s PID: %d STATE: %s LADDR: %s LPORT: %d RADDR: %s RPORT: %d\n", "C:\\\\?", t.DwOwningPid, winapi.TCPStatuses[s], winapi.ParseIPv4(t.DwLocalAddr), winapi.DecodePort(t.DwLocalPort), winapi.ParseIPv4(t.DwRemoteAddr), winapi.DecodePort(t.DwRemotePort))
+		} else {
+			buffer := make([]uint16, 257)
+			err := windows.GetModuleFileNameEx(handle, 0, &buffer[0], 256)
+			if err != nil {
+				results += fmt.Sprintf("EXE %s PID: %d STATE: %s LADDR: %s LPORT: %d RADDR: %s RPORT: %d\n", "C:\\\\?", t.DwOwningPid, winapi.TCPStatuses[s], winapi.ParseIPv4(t.DwLocalAddr), winapi.DecodePort(t.DwLocalPort), winapi.ParseIPv4(t.DwRemoteAddr), winapi.DecodePort(t.DwRemotePort))
+			} else {
+				exeName := windows.UTF16PtrToString(&buffer[0])
+				results += fmt.Sprintf("EXE %s PID: %d STATE: %s LADDR: %s LPORT: %d RADDR: %s RPORT: %d\n", exeName, t.DwOwningPid, winapi.TCPStatuses[s], winapi.ParseIPv4(t.DwLocalAddr), winapi.DecodePort(t.DwLocalPort), winapi.ParseIPv4(t.DwRemoteAddr), winapi.DecodePort(t.DwRemotePort))
+			}
+		}
 		index += step
 	}
 	return results, nil
